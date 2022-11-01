36f412ad2be7e8b204c318ba459e18e369143aed
JA-FSU <78230541+JA-FSU@users.noreply.github.com>
2022-11-01 15:15:52 -0400

    Added a random script, mostly for notes so I can remember stuff

---
+++ FinalProject3DGameDev/Assets/6_DumpFolders/Jonathan Dump/RandomStuff.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomStuff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Weasel asks for help because he can't move his arms. Never got an idle animation
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
