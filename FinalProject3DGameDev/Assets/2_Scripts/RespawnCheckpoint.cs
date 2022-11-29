4d93e415757333f802f6198d762ad2f5c3ea1429
Ronan-Cords <rcords@student.fitchburgstate.edu>
2022-11-29 15:49:46 -0500

    Respawn and Checkpoint Complete
    Checkpoint and Respawn system is done and working

--- FinalProject3DGameDev/Assets/2_Scripts/RespawnCheckpoint.cs
+++ FinalProject3DGameDev/Assets/2_Scripts/RespawnCheckpoint.cs
@@ -12,20 +12,33 @@ public class RespawnCheckpoint : MonoBehaviour
     public Transform PlayerTransform;
     public Transform RespawnTransform;
 
-    public float LevelBottom; 
+    public float LevelBottom;
+
+    Vector3 PlayerStart;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerStart = Player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Player.transform.position.y < LevelBottom)
         {
-            Player.transform.position = CheckpointTransform.position;
+            Player.transform.position = PlayerStart;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Checkpoint") && PlayerStart != other.transform.position)
+        {
+            PlayerStart = other.transform.position;
+
+            Debug.Log("TRIGGERED");
         }
     }
 }
